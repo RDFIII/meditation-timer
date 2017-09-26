143b90e363056c9f3c6735dde1fb59500f69b404
RDF <foulkesr@gmail.com>
2017-09-26 18:23:35 -0400

    evolving arcs

--- sketch.js
+++ sketch.js
@@ -1,9 +1,23 @@
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+	angleMode(DEGREES);
+	background(255, 204, 0);
 }
 
 function draw() {
-	
-	arc(windowWidth/2, windowHeight/2, 500, 500, 0, PI+QUARTER_PI, PIE);
 
+	let milliseconds = millis();
+
+	// noStroke();
+	// fill(204, 102, 0);
+	// ellipse(windowWidth/2, windowHeight/2, 500, 500);
+
+
+	noStroke();
+	fill(255, 204, 300);
+	arc(windowWidth/2, windowHeight/2, 500, 500, 0, milliseconds/100, PIE);
+
+	noStroke();
+	fill(255, 204, 300);
+	arc(windowWidth/2, windowHeight/2, 500, 500, 0, milliseconds/100, PIE);
 }
