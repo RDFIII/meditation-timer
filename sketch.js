b6c82fab9e145bcc1270a9a40e72e6a33c025359
RDF <foulkesr@gmail.com>
2017-09-29 10:33:53 -0400

    ellipse and arc now responsive to window size

--- sketch.js
+++ sketch.js
@@ -1,28 +1,34 @@
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	angleMode(DEGREES);
-	background(255, 204, 0);
+	background(210, 20, 20);
 }
 
 function draw() {
 
 	let milliseconds = millis();
 
-	noStroke();
-	fill(255, 204, 300);
-	ellipse(windowWidth/2, windowHeight/2, 700, 700);
+	// noStroke();
+	// fill(255, 255, 255);
+	// ellipse(windowWidth/2, windowHeight/2, 700, 700);
 
 	noStroke();
-	fill(255, 204, 0);
-	arc(windowWidth/2, windowHeight/2, 710, 710, -90, ((milliseconds/100) - 90), PIE);
+	fill(255, 255, 255);
+	ellipse(windowWidth/2, windowHeight/2, windowHeight/1.5, windowHeight/1.5);
 
 	noStroke();
-	fill(255, 204, 30);
-	ellipse(windowWidth/2, windowHeight/2, 150, 150);
+	fill(210, 20, 20);
+	arc(windowWidth/2, windowHeight/2, windowHeight/1.4, windowHeight/1.4, -90, ((milliseconds/100) - 90), PIE);
 
-	textAlign(CENTER);
-	textSize(32);
-	fill(255, 204, 300);
-	text(Math.floor(milliseconds/1000), windowWidth/2, windowHeight/2);
+	// Middle Circle
+	// noStroke();
+	// fill(255, 204, 30);
+	// ellipse(windowWidth/2, windowHeight/2, 150, 150);
+
+	// Timer
+	// textAlign(CENTER);
+	// textSize(32);
+	// fill(255, 204, 300);
+	// text(Math.floor(milliseconds/1000), windowWidth/2, windowHeight/2);
 
 }
